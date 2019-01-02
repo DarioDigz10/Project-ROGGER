6105bcf0004021866f2361da615d193952a30044
DarioDigz10 <46105884+DarioDigz10@users.noreply.github.com>
2019-01-02 14:41:54 +0100

    Prototype 0.7

---
+++ ROGGER/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject objeto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(objeto.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
