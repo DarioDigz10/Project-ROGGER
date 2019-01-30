2a7c0276bb0fcd3ebb893b296e89c50006c35bb1
Sanjillo <46110342+Sanjillo@users.noreply.github.com>
2019-01-30 12:11:45 +0100

    trampa de pinchos

---
+++ ROGGER/Assets/Scripts/Local/Traps/skewertrap.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class skewertrap : MonoBehaviour
+{
+    Player player;
+    [SerializeField] float damage;
+    [SerializeField] float trampaDesactivada;
+    private bool activada=false;
+    private float contador = 0;
+
+    void Start()
+    {
+        player = GameManager.Instance.Player;
+    }
+
+    private void Update()
+    {
+        if (activada == true && contador < trampaDesactivada)
+        {
+            contador = contador + Time.deltaTime;
+        }
+        else
+        {
+            activada = false;
+            contador = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        playerSetup ps = player.GetComponent<playerSetup>();
+        if (player.CompareTag("Player") && activada==false) {
+            ps.actualHealth -=damage;
+            activada = true;
+        }
+    }
+
+    
+}
