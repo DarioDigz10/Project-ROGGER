a9eb2bbc253b73bf35fff679600fc1680b9ea074
DarioDigz10 <46105884+DarioDigz10@users.noreply.github.com>
2019-02-04 11:01:55 +0100

    Enemy 2 A medias

---
+++ ROGGER/Assets/Scripts/Local/NPC/Enemy/Enemy 2/E2_Stun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class E2_Stun : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    void Start() {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    void Update() {
+        //if (animator.GetBool("Hit") == false) return;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            animator.SetBool("Hit", true);
+        }
+    }
+}
